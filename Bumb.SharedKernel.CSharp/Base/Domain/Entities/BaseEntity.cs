d8c6bccb526d0e4e857a2f195b2fab619889ef37
traitimtrongvang <pltvu.developer@outlook.com>
2024-02-08 20:19:46 +0700

    feat<base-domain>: create entity `BaseEntity`

---
+++ Bumb.SharedKernel.CSharp/Base/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using Bumb.SharedKernel.CSharp.Base.Domain.ValObjs.GuidVal;
+
+namespace Bumb.SharedKernel.CSharp.Base.Domain.Entities;
+
+public abstract class BaseEntity<TId> where TId : BaseGuidId<TId>
+{
+    public TId Id { get; init; } = (TId)Activator.CreateInstance(typeof(TId), Guid.NewGuid())!;
+
+    public DateTime CreatedAt { get; init; } = DateTime.Now;
+
+    protected BaseEntity()
+    { }
+
+    protected BaseEntity(TId id, DateTime createdAt)
+    {
+        Id = id;
+        CreatedAt = createdAt;
+    }
+}
