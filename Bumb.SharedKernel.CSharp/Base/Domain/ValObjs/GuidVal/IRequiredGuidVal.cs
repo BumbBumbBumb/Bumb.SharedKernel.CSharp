45bfae2b4369e7132fc563985e9f9e9036c10137
traitimtrongvang <pltvu.developer@outlook.com>
2024-02-08 20:07:14 +0700

    feat<base-domain>: create value object interface `IRequiredGuidVal`

---
+++ Bumb.SharedKernel.CSharp/Base/Domain/ValObjs/GuidVal/IRequiredGuidVal.cs
@@ -0,0 +1,12 @@
+﻿using Bumb.SharedKernel.CSharp.Base.Domain.Exceptions.GuidVal;
+
+namespace Bumb.SharedKernel.CSharp.Base.Domain.ValObjs.GuidVal;
+
+public interface IRequiredGuidVal<TValObj>
+{
+    public static void ValidateAndThrow(Guid val)
+    {
+        if (val.Equals(Guid.Empty))
+            throw new InvalidRequiredGuidValException<TValObj>();
+    }
+}
