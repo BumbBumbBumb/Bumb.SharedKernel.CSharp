18cb71b772ecafafc30beabc3360903318227ef0
traitimtrongvang <pltvu.developer@outlook.com>
2024-02-08 20:19:05 +0700

    feat<base-domain>: create value object `BaseGuidVal`

---
+++ Bumb.SharedKernel.CSharp/Base/Domain/ValObjs/GuidVal/BaseGuidVal.cs
@@ -0,0 +1,10 @@
+﻿namespace Bumb.SharedKernel.CSharp.Base.Domain.ValObjs.GuidVal;
+
+public abstract record BaseGuidVal(Guid Val)
+{
+    public static implicit operator string(BaseGuidVal val)
+        => val.ToString();
+    
+    public override string ToString()
+        => Val.ToString();
+}
