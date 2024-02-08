e2616001f5f64aa7a251d4033269a1de4b481a3b
traitimtrongvang <pltvu.developer@outlook.com>
2024-02-08 20:19:13 +0700

    feat<base-domain>: create value object `BaseGuidId`

---
+++ Bumb.SharedKernel.CSharp/Base/Domain/ValObjs/GuidVal/BaseGuidId.cs
@@ -0,0 +1,14 @@
+﻿namespace Bumb.SharedKernel.CSharp.Base.Domain.ValObjs.GuidVal;
+
+public abstract record BaseGuidId<TDerived> : BaseGuidVal
+{
+    public static void ValidateAndThrow(Guid val)
+    {
+        IRequiredGuidVal<TDerived>.ValidateAndThrow(val);
+    }
+    
+    protected BaseGuidId(Guid val) : base(val)
+    {
+        ValidateAndThrow(val);
+    }
+}
