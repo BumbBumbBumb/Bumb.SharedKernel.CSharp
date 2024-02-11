c1e5bfaedf520064371dea566e845fe70e5f999b
traitimtrongvang <pltvu.developer@outlook.com>
2024-02-11 22:01:53 +0700

    feat: new features, who know??? hahaha

---
+++ Bumb.SharedKernel.CSharp/Base/Domain/Exceptions/StringVal/InvalidMaxLengthStringValExc.cs
@@ -0,0 +1,9 @@
+﻿using Bumb.SharedKernel.CSharp.Base.Domain.Exceptions.Base;
+
+namespace Bumb.SharedKernel.CSharp.Base.Domain.Exceptions.StringVal;
+
+public class InvalidMaxLengthStringValExc<TValObj>(string value)
+    : BaseException<InvalidMinLengthStringValExc<TValObj>>, IExcHasValue<string>
+{
+    public string Value { get; } = value;
+}
---
+++ Bumb.SharedKernel.CSharp/Base/Domain/ValObjs/StringVal/IHasMaxLengthStringVal.cs
@@ -0,0 +1,14 @@
+﻿using Bumb.SharedKernel.CSharp.Base.Domain.Exceptions.StringVal;
+
+namespace Bumb.SharedKernel.CSharp.Base.Domain.ValObjs.StringVal;
+
+public interface IHasMaxLengthStringVal<TValObj>
+{
+    public static abstract int MaxLength { get; }
+
+    public static void ValidateAndThrow(string val, int maxLength)
+    {
+        if (val.Length > maxLength)
+            throw new InvalidMaxLengthStringValExc<TValObj>(val);
+    }
+}
