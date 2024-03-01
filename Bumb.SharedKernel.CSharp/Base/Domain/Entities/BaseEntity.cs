4915c577c45128ba4f0ef468258d54d24514b34b
traitimtrongvang <pltvu.developer@outlook.com>
2024-03-01 21:38:12 +0700

    yah 2.0.6

--- Bumb.SharedKernel.CSharp/Base/Domain/Entities/BaseEntity.cs
+++ Bumb.SharedKernel.CSharp/Base/Domain/Entities/BaseEntity.cs
@@ -1,7 +1,22 @@
-﻿namespace Bumb.SharedKernel.CSharp.Base.Domain.Entities;
+﻿using Bumb.SharedKernel.CSharp.Base.Domain.Rules.BaseEntityRules;
+using Bumb.SharedKernel.CSharp.Base.Domain.Validation;
+
+namespace Bumb.SharedKernel.CSharp.Base.Domain.Entities;
 
 public abstract class BaseEntity
 {
     public Guid Id { get; init; } = Guid.NewGuid();
     public DateTime CreatedAt { get; init; } = DateTime.Now;
+}
+
+public class BaseEntityRule : BaseValidator<BaseEntity, BaseEntityRule>
+{
+    public BaseEntityRule()
+    {
+        RuleFor(e => e.Id)
+            .SetValidator(GuidIdRule.Instance);
+
+        RuleFor(e => e.CreatedAt)
+            .SetValidator(CreatedAtRule.Instance);
+    }
 }
